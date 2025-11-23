5b069afb2f0e53a95eb99935369fc8d98d1e4b2a
Claus <clausemberg@yahoo.com.br>
2025-11-23 06:37:25 -0300

    inserindo ads

--- backend/app/api/endpoints/ads.py
+++ backend/app/api/endpoints/ads.py
@@ -21,6 +21,7 @@ router = APIRouter()
 
 @router.post("/ad-contents", response_model=AdContentResponse, status_code=status.HTTP_201_CREATED)
 async def create_ad_content(
+   
     ad_content: AdContentCreate,
     current_user: User = Depends(get_current_user),
     db: AsyncIOMotorDatabase = Depends(get_database)
